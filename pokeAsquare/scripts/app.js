680f364ac2c68c285f8d3a971f8440521a0bc5a2
MarchWaden <max.linz@live.com>
2018-09-21 10:32:57 -0600

    wrote create_squares

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,1 +1,12 @@
 
+$('button').on('click', () => {
+  console.log('click works');
+});
+
+create_squares = (number) => {
+  for(i=0;i<number;i++){
+    const color = ("blue_square");
+    $('.squares').append(`<div class="${color}">  hiuhi   </div>`);
+  }
+}
+create_squares(30);
